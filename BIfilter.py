4e61bd9d7a27646da0d0afddf4b60bf5ee68d38e
Yifan Chen <yifan.i.chen@gmail.com>
2021-08-27 14:49:29 -0400

    fix change pct logic

--- BIfilter.py
+++ BIfilter.py
@@ -1,4 +1,4 @@
-def filter(configs=None, vol=None, price=None, change_pct=None, btc_price=None):
+def filter(configs=None, vol=None, price=None, btc_price=None):
     if configs['DAILY_VOLUME']:
         if not (configs['DAILY_VOLUME_MIN'] <= vol <= configs['DAILY_VOLUME_MAX']):
             return False
@@ -9,10 +9,6 @@ def filter(configs=None, vol=None, price=None, change_pct=None, btc_price=None):
 
     if configs['DOLLAR_PRICE']:
         if not (configs['DOLLAR_PRICE_MIN'] <= price * btc_price <= configs['DOLLAR_PRICE_MAX']):
-            return False
-
-    if configs['CHANGE_PCT']:
-        if not (configs['CHANGE_PCT_MIN'] <= change_pct <= configs['CHANGE_PCT_MAX']):
             return False
 
     return True
